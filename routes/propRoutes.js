5f2e3afac245b55e5f043f7c7bc0d99bf7e20d53
bmcgrane302 <bmcgrane302@yahoo.com>
2017-12-11 16:42:25 -0700

    adjusted post route

--- routes/propRoutes.js
+++ routes/propRoutes.js
@@ -6,119 +6,143 @@ var knex = require('../db/knex');
 router.get('/', (req, res) => {
   knex('properties')
     .then(function(property) {
-    res.send(property);
-  });
+      res.send(property);
+    });
 });
 //add property
-router.post('/', (req,res) => {
+router.post('/', (req, res) => {
   knex('properties')
-   .insert(req.body)
-   .then(
-     knex('properties')
-     .select()
-       .then(property => res.send(property))
-
-   )
+    .insert(req.body)
+    .then(() => {
+      knex('properties')
+        .select()
+        .then((property) => {
+          console.log(property);
+          res.send(property)
+        })
+    })
 })
 //delete property
-router.delete('/:id', (req,res) => {
+router.delete('/:id', (req, res) => {
   console.log("PARAMS", req.params)
   knex('properties')
-   .del()
-   .where('id', req.params.id)
-    .then(
+    .del()
+    .where('id', req.params.id)
+    .then(() => {
       knex('properties')
-      .select()
+        .select()
         .where('id', req.params.id)
         .then(property => res.send(property))
-
-
-    )
+    })
 })
 //update property
-router.patch('/:id/update', (req, res) =>{
+router.patch('/:id/update', (req, res) => {
   knex('properties')
-   .update(req.body)
-   .where('id', req.params.id)
-   .then(
-     knex('properties')
-     .select()
-       .where('id', req.params.id)
-       .then(property => res.send(property))
-
-   )
+    .update(req.body)
+    .where('id', req.params.id)
+    .then(() => {
+        knex('properties')
+          .select()
+          .where('id', req.params.id)
+          .then(property => res.send(property))
+      }
+    )
 })
 //rent paid true
 router.patch('/:id/paid', (req, res) => {
   knex('properties')
-   .update({rent_paid: true})
-   .where('id', req.params.id)
-   .then(
-     knex('properties')
-     .select()
-       .where('id', req.params.id)
-       .then(property => res.send(property))
-   )
+    .update({
+      rent_paid: true
+    })
+    .where('id', req.params.id)
+    .then(()=>{
+      knex('properties')
+      .select()
+      .where('id', req.params.id)
+      .then(property => res.send(property))
+    }
+
+    )
 })
 //rent paid false
 router.patch('/:id/unpaid', (req, res) => {
   knex('properties')
-   .update({rent_paid: false})
-   .where('id', req.params.id)
-   .then(
-     knex('properties')
-     .select()
+    .update({
+      rent_paid: false
+    })
+    .where('id', req.params.id)
+    .then(()=>{
+      knex('properties')
+      .select()
       .where('id', req.params.id)
       .then(property => res.send(property))
-   )
+    }
+
+    )
 })
 //add repair
 router.patch('/:id/addrepair', (req, res) => {
   knex('properties')
-   .update({repairs: true})
-   .where('id', req.params.id)
-   .then(
-     knex('properties')
-     .select()
+    .update({
+      repairs: true
+    })
+    .where('id', req.params.id)
+    .then(()=>{
+      knex('properties')
+      .select()
       .where('id', req.params.id)
       .then(property => res.send(property))
-   )
+    }
+
+    )
 })
 //remove repair
 router.patch('/:id/removerepair', (req, res) => {
   knex('properties')
-   .update({repairs: false})
-   .where('id', req.params.id)
-   .then(
-     knex('properties')
-    .select()
-     .where('id', req.params.id)
-     .then(property => res.send(property))
-   )
+    .update({
+      repairs: false
+    })
+    .where('id', req.params.id)
+    .then(()=>{
+      knex('properties')
+      .select()
+      .where('id', req.params.id)
+      .then(property => res.send(property))
+    }
+
+    )
 })
 //renewal to true
 router.patch('/:id/addrenewal', (req, res) => {
   knex('properties')
-   .update({renewal_notice: true})
-   .where('id', req.params.id)
-   .then(
-     knex('properties')
-    .select()
+    .update({
+      renewal_notice: true
+    })
+    .where('id', req.params.id)
+    .then(()=>{
+      knex('properties')
+      .select()
       .where('id', req.params.id)
       .then(property => res.send(property))
-   )
+    }
+
+    )
 })
 
 router.patch('/:id/removerenewal', (req, res) => {
   knex('properties')
-   .update({renewal_notice: false})
-   .where('id', req.params.id)
-   .then(
-     knex('properties')
-     .select()
+    .update({
+      renewal_notice: false
+    })
+    .where('id', req.params.id)
+    .then(()=>{
+      knex('properties')
+      .select()
       .where('id', req.params.id)
       .then(property => res.send(property))
-   )
+    }
+
+    )
 })
 
 
